40f771c155ce7e95bf3bcbbe16b7896a1bd9ef8c
Peter Noble <petershaggynoble@gmail.com>
2019-03-11 16:30:54 +0000

    Compressor updates
    Improves the sorting of compressor results by factoring in delimiters with shortcuts & bases with constants, and appends splitting on delimiter (shortcut) and base mapping to output string. Also adds undo & redo icons, removes safe mode, increases the prominence of the run icon and fixes various bugs.

--- js/default.js
+++ js/default.js
@@ -27,67 +27,50 @@ const 	W=window,
 		timer:I(`timer`),
 		transpiled:I(`transpiled`)
 	},
-	variables={
-		bytes:0,
-		permute:false,
-		running:false,
-		safemode:false
-	},
 	compressor={
-		alphabet:`\nabcdefghijklmnopqrstuvwxyz`,
+		permute:false,
 		bases:[16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36],
+		delimiters:` \nabcdefghijklmnopqrstuvwxyz`,
+		golfables:`gnpqswy`,
+		shortcuts:`ÎÍ²¬¤ÔÕ`,
 		buttons:{
 			compress:I(`compress`),
 			copy:I(`copy_compressed`),
 			permute:I(`permute`),
 			reset:I(`reset`)
 		},
-		compress(){
+		init(){
 			compressor.value=fields.compressor.value;
 			if(compressor.original=compressor.value){
-				try{
-					compressor.value=eval(compressor.value);
-					if(compressor.value.constructor===String)
-						compressor.result=[{string:shoco.c(compressor.value)}];
-					else if(compressor.value.constructor===Array){
-						if(compressor.value.every(x=>x.constructor===String)){
-							if(variables.permute)
-								compressor.value=compressor.value.á();
-							compressor.result=compressor.map(compressor.value);
-						}else if(compressor.value.every(x=>x.constructor===Number)){
-							if(variables.permute)
-								compressor.value=compressor.value.á();
-							compressor.result=compressor.bases.map(base=>compressor.get(compressor.map(compressor.value,base)));
-						}
-					}
-					for(let result of compressor.result)
-						result.string=`\`${result.string.replace(/`/g,"\\`")}\``;
-					compressor.result=compressor.get(compressor.result);
-					compressor.update();
-				}catch(err){
-					console.error(err);
-				}
+				compressor.buttons.compress.classList.add(`dn`);
+				compressor.buttons.permute.classList.add(`dn`);
+				compressor.buttons.reset.classList.remove(`dn`);
+				if(compressor.permute)
+					compressor.buttons.reset.classList.add(`spin`);
+				fields.compressor.readOnly=true;
+				setTimeout(compressor.run);
 			}
 		},
 		get(array){
-			return array.sort((one,two)=>one.string.length-two.string.length)[0];
+			return array.sort((one,two)=>compressor.sort(one)-compressor.sort(two))[0];
 		},
 		map(array,base){
 			if(array[0].constructor===Array)
 				return array.map(array=>compressor.get(compressor.map(array,base)));
-			let alphabet,strings;
+			let delimiters,strings;
 			if(base){
 				strings=array.map(num=>num.toString(base));
-				alphabet=compressor.alphabet.k(strings.join(``));
-			}else alphabet=compressor.alphabet.k(array.join(``));
-			return [...alphabet].map(character=>{
-				let obj={delimiter:character===`\n`?`R`:`"${character}"`};
-				if(variables.permute)
+				delimiters=compressor.delimiters.k(strings.join(``));
+			}else delimiters=compressor.delimiters.k(array.join(``));
+			return [...delimiters].map(character=>{
+				let obj={delimiter:character};
+				if(compressor.permute)
 					obj.permutation=array;
 				if(base){
 					obj.string=shoco.c(strings.join(character));
-					obj.base=base%16?base:`GH`[base/16];
+					obj.base=base;
 				}else obj.string=shoco.c(array.join(character));
+				obj.string=`\`${obj.string.replace(/`/g,"\\`")}\``;
 				return obj;
 			});
 		},
@@ -100,34 +83,77 @@ const 	W=window,
 			compressor.buttons.copy.classList.add(`dn`);
 			compressor.buttons.reset.classList.add(`dn`);
 		},
+		run(){
+			if(compressor.original=compressor.value){
+				try{
+					compressor.value=eval(compressor.value);
+					if(compressor.value.constructor===String)
+						compressor.result={string:`\`${shoco.c(compressor.value).replace(/`/g,"\\`")}\``};
+					else if(compressor.value.constructor===Array){
+						if(compressor.value.every(x=>x.constructor===String)){
+							if(compressor.permute)
+								compressor.value=compressor.value.á();
+							compressor.result=compressor.map(compressor.value);
+						}else if(compressor.value.every(x=>x.constructor===Number)){
+							if(compressor.permute)
+								compressor.value=compressor.value.á();
+							compressor.result=compressor.bases.map(base=>compressor.map(compressor.value,base));
+							compressor.result=compressor.result.map(compressor.get);
+						}
+						compressor.result=compressor.get(compressor.result);
+					}
+					compressor.update();
+				}catch(err){
+					console.error(err);
+					compressor.reset();
+				}
+			}
+		},
+		sort(obj){
+			return obj.string.length-` \n`.includes(obj.delimiter)-` \n${compressor.golfables}`.includes(obj.delimiter)-[16,32].includes(obj.base);
+		},
 		update(){
-			compressor.buttons.compress.classList.add(`dn`);
-			compressor.buttons.permute.classList.add(`dn`);
-			compressor.buttons.copy.classList.remove(`dn`);
-			compressor.buttons.reset.classList.remove(`dn`);
-			fields.compressor.readOnly=true;
+			if(compressor.result.delimiter){
+				if(compressor.result.delimiter===` `){
+					compressor.result.delimiter=`S`;
+					compressor.result.string+=`¸`;
+				}
+				else if(compressor.result.delimiter===`\n`){
+					compressor.result.delimiter=`R`;
+					compressor.result.string+=`·`;
+				}else if(compressor.golfables.includes(compressor.result.delimiter))
+					compressor.result.string+=`q${compressor.shortcuts[compressor.golfables.search(compressor.result.delimiter)]}`;
+				else compressor.result.string+=`q${compressor.result.delimiter} `;
+			}
+			if(compressor.result.base)
+				compressor.result.string+=`mn${compressor.result.base=compressor.result.base%16?compressor.result.base:`GH`[compressor.result.base/16-1]} `;
 			fields.compressor.value=`Original:    ${compressor.original}\n`;
 			if(compressor.result.permutation)
 				fields.compressor.value+=`Permutation: ${JSON.stringify(compressor.result.permutation)}\n`;
-			fields.compressor.value+=`Bytes:       ${compressor.original.length}\nCompressed:  ${compressor.result.string}\n`;
+			fields.compressor.value+=`Bytes:       ${compressor.original.length}\n`;
+			fields.compressor.value+=`Compressed:  ${compressor.result.string}\n`;
 			if(compressor.result.base)
 				fields.compressor.value+=`Base:        ${compressor.result.base}\n`;
 			if(compressor.result.delimiter)
 				fields.compressor.value+=`Delimiter:   ${compressor.result.delimiter}\n`;
 			fields.compressor.value+=`Bytes:       ${compressor.result.string.length}`;
 			fields.compressor.dispatchEvent(new Event(`input`));
+			compressor.buttons.copy.classList.remove(`dn`);
+			if(compressor.permute)
+				compressor.buttons.reset.addEventListener(`animationiteration`,()=>compressor.buttons.reset.classList.remove(`spin`),false);
 		}
 	},
 	docs={
 		sidebar:I(`docs`),
 		init(){
-			let 	headings=docs.sidebar.querySelectorAll(`h3`),
-				length=headings.length,
+			let 	articles=docs.sidebar.querySelectorAll(`article`),
+				length=articles.length,
 				list=E(`ol`),
-				heading,item,menu,svg;
+				article,heaing,item,menu,svg;
 			list.classList.add(`pa`);
 			list.tabIndex=`-1`;
-			for(heading of headings){
+			for(article of articles){
+				heading=article.firstElementChild;
 				if(svg){
 					svg=svg.cloneNode(1);
 					item=item.cloneNode(1);
@@ -143,14 +169,14 @@ const 	W=window,
 					item.append(T(heading.firstChild.nodeValue));
 				}
 				heading.append(svg);
-				heading.style.zIndex=length--;
-				item.dataset.for=heading.id;
+				article.style.zIndex=length--;
+				item.dataset.for=article.id;
 				list.append(item);
 			}
-			for(heading of headings){
+			for(article of articles){
 				menu=list.cloneNode(1);
-				menu.querySelector(`[data-for=${heading.id}]`).remove();
-				heading.append(menu);
+				menu.querySelector(`[data-for=${article.id}]`).remove();
+				article.firstElementChild.append(menu);
 			}
 			docs.shortcuts();
 		},
@@ -161,7 +187,7 @@ const 	W=window,
 			if(event.target.dataset.for){
 				event.currentTarget.scroll({
 					behavior:`smooth`,
-					top:I(event.target.dataset.for).parentNode.offsetTop
+					top:I(event.target.dataset.for).offsetTop
 				});
 				event.target.parentNode.blur();
 			}
@@ -205,8 +231,8 @@ const 	W=window,
 		}
 	},
 	interpreter={
-		decode:text=>atob(text).replace(/\\u[0-9A-Fa-f]{4}/g,chr=>String.fromCodePoint(parseInt(chr.slice(2),16))),
-		encode:text=>btoa(text.replace(/./g,chr=>chr.charCodeAt(0)<256?chr:`\\u`+(`00`+chr.charCodeAt(0).toString(16)).slice(-4))),
+		bytes:0,
+		running:false,
 		init(){
 			let params=U.searchParams
 			versions.selected=versions.numbers.includes(params.get(`v`))?params.get(`v`):versions.current;
@@ -224,6 +250,12 @@ const 	W=window,
 			fields.counter.append(T(``));
 			fields.timer.append(T(``));
 		},
+		decode(text){
+			return atob(text).replace(/\\u[0-9A-Fa-f]{4}/g,chr=>String.fromCodePoint(parseInt(chr.slice(2),16)));
+		},
+		encode(text){
+			return btoa(text.replace(/./g,chr=>chr.charCodeAt(0)<256?chr:`\\u`+(`00`+chr.charCodeAt(0).toString(16)).slice(-4)));
+		},
 		golf(){
 			let 	value=fields.code.value,
 				golfed=golfables.reduce((output,string,index)=>output.replace(RegExp(string.replace(/(?=\W)/g,`\\`),`g`),shortcuts[index]),value);
@@ -240,26 +272,25 @@ const 	W=window,
 				markdown+=` v`+versions.selected
 			if(fields.flags.value)
 				markdown+=` [\`${fields.flags.value}\`](https://codegolf.meta.stackexchange.com/a/14339/)`;
-			markdown+=`, ${variables.bytes} ${`bytes`.slice(0,variables.bytes!==1?5:4)}\n\n${fields.code.value.replace(/^/gm,`    `)}\n\n[Try it](${interpreter.url()})`;
+			markdown+=`, ${interpreter.bytes} ${`bytes`.slice(0,interpreter.bytes!==1?5:4)}\n\n${fields.code.value.replace(/^/gm,`    `)}\n\n[Try it](${interpreter.url()})`;
 			if(fields.explanation.value)
-				markdown+=`\n\n-----\n\n##Explanation\n\n`+fields.explanation.value.replace(/^/gm,`    `)
+				markdown+=`\n\n`+fields.explanation.value.replace(/^/gm,`    `)
 			return markdown
 		},
 		run(){
-			if(!variables.running&&Japt){
+			if(!interpreter.running&&Japt){
 				let input,timer;
 				input=fields.input.value;
 				if(fields.flags.value)
 					input+=`\n`+fields.flags.value;
-				variables.running=true;
+				interpreter.running=true;
 				B.classList.add(`cw`);
-				B.dataset.running=`true`;
 				fields.error.parentNode.classList.add(`dn`);
 				fields.timer.firstChild.nodeValue=``;
 				Japt.run(
 					fields.code.value,
 					input,
-					variables.safemode,
+					false,//safemode
 					()=>timer=performance.now(),
 					result=>{
 						fields.timer.firstChild.nodeValue=((performance.now()-timer)/1e3).toFixed(3)+` seconds`;
@@ -269,14 +300,13 @@ const 	W=window,
 						}
 						B.dataset.running=`false`;
 						B.classList.remove(`cw`);
-						variables.running=false;
+						interpreter.running=false;
 					},
 					err=>{
 						Japt.error(err);
 						fields.error.parentNode.classList.remove(`dn`);
-						B.dataset.running=`false`;
 						B.classList.remove(`cw`);
-						variables.running=false;
+						interpreter.running=false;
 					}
 				);
 			}
@@ -293,8 +323,8 @@ const 	W=window,
 				code=unescape(encodeURI(code));
 				encoding=`UTF-8`;
 			}
-			variables.bytes=code.length;
-			fields.counter.firstChild.nodeValue=variables.bytes+` bytes`.slice(0,variables.bytes!==1?6:5)+` (${encoding})`;
+			interpreter.bytes=code.length;
+			fields.counter.firstChild.nodeValue=interpreter.bytes+` bytes`.slice(0,interpreter.bytes!==1?6:5)+` (${encoding})`;
 		},
 		url(version=versions.selected){
 			let url=U.protocol+`//${U.hostname+U.pathname}?v=`+version;
@@ -413,17 +443,17 @@ const 	W=window,
 		copy(text){
 			fields.clipboard.value=text;
 			fields.clipboard.select();
-			D.execCommand(`copy`);
+			D.execCommand(`copy`,false);
 		},
 		icons(){
 			let 	library={
 					"alert":`M13,14H11V10H13M13,18H11V16H13M1,21H23L12,2L1,21Z`,
 					"arrow-collapse":`M19.5,3.09L15,7.59V4H13V11H20V9H16.41L20.91,4.5L19.5,3.09M4,13V15H7.59L3.09,19.5L4.5,20.91L9,16.41V20H11V13H4Z`,
+					"autorenew":`M12,6V9L16,5L12,1V4A8,8 0 0,0 4,12C4,13.57 4.46,15.03 5.24,16.26L6.7,14.8C6.25,13.97 6,13 6,12A6,6 0 0,1 12,6M18.76,7.74L17.3,9.2C17.74,10.04 18,11 18,12A6,6 0 0,1 12,18V15L8,19L12,23V20A8,8 0 0,0 20,12C20,10.43 19.54,8.97 18.76,7.74Z`,
 					"clipboard-text-outline":`M19,4H14.82C14.25,2.44 12.53,1.64 11,2.2C10.14,2.5 9.5,3.16 9.18,4H5A2,2 0 0,0 3,6V20A2,2 0 0,0 5,22H19A2,2 0 0,0 21,20V6A2,2 0 0,0 19,4M12,4A1,1 0 0,1 13,5A1,1 0 0,1 12,6A1,1 0 0,1 11,5A1,1 0 0,1 12,4M7,8H17V6H19V20H5V6H7V8M17,12H7V10H17V12M15,16H7V14H15V16Z`,
 					"dots-vertical":`M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z`,
 					"golf":`M19.5,18A1.5,1.5 0 0,1 21,19.5A1.5,1.5 0 0,1 19.5,21A1.5,1.5 0 0,1 18,19.5A1.5,1.5 0 0,1 19.5,18M17,5.92L11,9V18.03C13.84,18.19 16,19 16,20C16,21.1 13.31,22 10,22C6.69,22 4,21.1 4,20C4,19.26 5.21,18.62 7,18.27V20H9V2L17,5.92Z`,
 					"forum":`M17,12V3A1,1 0 0,0 16,2H3A1,1 0 0,0 2,3V17L6,13H16A1,1 0 0,0 17,12M21,6H19V15H6V17A1,1 0 0,0 7,18H18L22,22V7A1,1 0 0,0 21,6Z`,
-					"help-circle-outline":`M11,18H13V16H11V18M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,6A4,4 0 0,0 8,10H10A2,2 0 0,1 12,8A2,2 0 0,1 14,10C14,12 11,11.75 11,15H13C13,12.75 16,12.5 16,10A4,4 0 0,0 12,6Z`,
 					"information":`M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z`,
 					"flag":`M14.4,6L14,4H5V21H7V14H12.6L13,16H20V6H14.4Z`,
 					"file-document":`M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M15,18V16H6V18H15M18,14V12H6V14H18Z`,
@@ -431,14 +461,11 @@ const 	W=window,
 					"invert-colors":`M12,19.58V19.58C10.4,19.58 8.89,18.96 7.76,17.83C6.62,16.69 6,15.19 6,13.58C6,12 6.62,10.47 7.76,9.34L12,5.1M17.66,7.93L12,2.27V2.27L6.34,7.93C3.22,11.05 3.22,16.12 6.34,19.24C7.9,20.8 9.95,21.58 12,21.58C14.05,21.58 16.1,20.8 17.66,19.24C20.78,16.12 20.78,11.05 17.66,7.93Z`,
 					"keyboard":`M19,10H17V8H19M19,13H17V11H19M16,10H14V8H16M16,13H14V11H16M16,17H8V15H16M7,10H5V8H7M7,13H5V11H7M8,11H10V13H8M8,8H10V10H8M11,11H13V13H11M11,8H13V10H11M20,5H4C2.89,5 2,5.89 2,7V17A2,2 0 0,0 4,19H20A2,2 0 0,0 22,17V7C22,5.89 21.1,5 20,5Z`,
 					"link-variant":`M10.59,13.41C11,13.8 11,14.44 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C7.22,12.88 7.22,9.71 9.17,7.76V7.76L12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.59,9.17C9.41,10.34 9.41,12.24 10.59,13.41M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.78,11.12 16.78,14.29 14.83,16.24V16.24L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L13.41,14.83C14.59,13.66 14.59,11.76 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z`,
-					"loading":`M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z`,
-					"lock":`M12,17A2,2 0 0,0 14,15C14,13.89 13.1,13 12,13A2,2 0 0,0 10,15A2,2 0 0,0 12,17M18,8A2,2 0 0,1 20,10V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V10C4,8.89 4.9,8 6,8H7V6A5,5 0 0,1 12,1A5,5 0 0,1 17,6V8H18M12,3A3,3 0 0,0 9,6V8H15V6A3,3 0 0,0 12,3Z`,
-					"lock-open":`M18,8A2,2 0 0,1 20,10V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V10A2,2 0 0,1 6,8H15V6A3,3 0 0,0 12,3A3,3 0 0,0 9,6H7A5,5 0 0,1 12,1A5,5 0 0,1 17,6V8H18M12,17A2,2 0 0,0 14,15A2,2 0 0,0 12,13A2,2 0 0,0 10,15A2,2 0 0,0 12,17Z`,
 					"menu-down":`M7,10L12,15L17,10H7Z`,
 					"play-circle":`M10,16.5V7.5L16,12M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z`,
+					"redo":`M18.4,10.6C16.55,9 14.15,8 11.5,8C6.85,8 2.92,11.03 1.54,15.22L3.9,16C4.95,12.81 7.95,10.5 11.5,10.5C13.45,10.5 15.23,11.22 16.62,12.38L13,16H22V7L18.4,10.6Z`,
 					"shuffle":`M14.83,13.41L13.42,14.82L16.55,17.95L14.5,20H20V14.5L17.96,16.54L14.83,13.41M14.5,4L16.54,6.04L4,18.59L5.41,20L17.96,7.46L20,9.5V4M10.59,9.17L5.41,4L4,5.41L9.17,10.58L10.59,9.17Z`,
 					"shuffle-disabled":`M16,4.5V7H5V9H16V11.5L19.5,8M16,12.5V15H5V17H16V19.5L19.5,16`,
-					"stop-circle":`M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M9,9H15V15H9`,
 					"text":`M21,6V8H3V6H21M3,18H12V16H3V18M3,13H21V11H3V13Z`,
 					"undo":`M12.5,8C9.85,8 7.45,9 5.6,10.6L2,7V16H11L7.38,12.38C8.77,11.22 10.54,10.5 12.5,10.5C16.04,10.5 19.05,12.81 20.1,16L22.47,15.22C21.08,11.03 17.15,8 12.5,8Z`
 				},icon,path,svg;
@@ -452,7 +479,7 @@ const 	W=window,
 						path=N(`path`);
 						path.classList.add(`pen`);
 					}
-					path.setAttribute(`d`,library[icon]||library[`help-circle-outline`]);
+					path.setAttribute(`d`,library[icon]);
 					svg.append(path);
 				}
 			}
@@ -462,19 +489,20 @@ const 	W=window,
 			I(`theme`).addEventListener(`click`,theme,false);
 			fields.flags.addEventListener(`focus`,()=>{if(!fields.flags.value)fields.flags.value=`-`;},false);
 			fields.flags.addEventListener(`blur`,()=>{if(fields.flags.value===`-`)fields.flags.value=``;},false);
-			I(`run`).addEventListener(`click`,()=>!variables.running?interpreter.run():interpreter.stop(),false);
-			I(`safemode`).addEventListener(`click`,()=>B.dataset.safemode=(variables.safemode=!variables.safemode),false);
+			I(`run`).addEventListener(`click`,()=>!interpreter.running?interpreter.run():interpreter.stop(),false);
 			I(`copy_link`).addEventListener(`click`,()=>general.copy(interpreter.url()),false);
 			I(`copy_post`).addEventListener(`click`,()=>general.copy(interpreter.markdown()),false);
 			I(`theme`).addEventListener(`click`,general.theme,false);
+			I(`undo`).addEventListener(`click`,()=>D.execCommand(`undo`,false),false);
+			I(`redo`).addEventListener(`click`,()=>D.execCommand(`redo`,false),false);
 			I(`golf`).addEventListener(`click`,interpreter.golf,false);
 			I(`copy_code`).addEventListener(`click`,()=>general.copy(fields.code.value),false);
 			fields.code.addEventListener(`input`,interpreter.update,false);
 			fields.input.addEventListener(`input`,()=>general.resize(fields.input),false);
 			I(`copy_explanation`).addEventListener(`click`,()=>general.copy(fields.explanation.value.replace(/^/gm,`    `)),false);
 			fields.explanation.addEventListener(`input`,()=>general.resize(fields.explanation),false);
-			compressor.buttons.compress.addEventListener(`click`,compressor.compress,false);
-			compressor.buttons.permute.addEventListener(`click`,()=>B.dataset.permute=(variables.permute=!variables.permute),false);
+			compressor.buttons.compress.addEventListener(`click`,compressor.init,false);
+			compressor.buttons.permute.addEventListener(`click`,()=>compressor.buttons.permute.classList.toggle(`enabled`,compressor.permute=!compressor.permute),false);
 			compressor.buttons.copy.addEventListener(`click`,()=>general.copy(compressor.result.string),false);
 			compressor.buttons.reset.addEventListener(`click`,compressor.reset,false);
 			fields.compressor.addEventListener(`input`,()=>general.resize(fields.compressor),false);
@@ -483,7 +511,6 @@ const 	W=window,
 			docs.sidebar.lastElementChild.addEventListener(`click`,keyboard.insert,false);
 			Q(`#keyboard>h2`).addEventListener(`click`,keyboard.toggle,false);
 			keyboard.list.addEventListener(`click`,keyboard.insert,false);
-			D.addEventListener(`scroll`,general.scroll,false);
 			W.addEventListener(`resize`,()=>{
 				general.resize(fields.code);
 				general.resize(fields.transpiled);
@@ -496,9 +523,6 @@ const 	W=window,
 		resize(field){
 			fields.clipboard.value=field.value;
 			field.style.height=(fields.clipboard.scrollHeight)+`px`;
-		},
-		scroll(){
-			B.dataset.header=(B.scrollTop||H.scrollTop||W.pageYOffset||0)<51;
 		},
 		theme(){
 			B.classList.toggle(`light`);
